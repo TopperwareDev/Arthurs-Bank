eb146059c179ae9e1c5bbe5a97a7f8088378af41
Topperware <47272494+TopperwareDev@users.noreply.github.com>
2021-10-24 17:29:56 +0200

    Further js intergration
    I did some work and experimanetation  with the js files in the login page

---
+++ Web-HTML-Public/UserMenu/User_Menu.js
@@ -0,0 +1,6 @@
+
+//document.getElementById('test').onclick = function(event) {
+//    document.getElementById('test').className = "hidden";
+//  }
+
+console.log('ahsdahdskajsdhaskdjaskdjahsdkjjjksdf');
