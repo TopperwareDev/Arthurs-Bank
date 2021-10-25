dbc284c2460b0a73a62bc923677f42d4beb09e16
PurpleToenail <nickgur.pass@gmail.com>
2021-10-25 14:44:15 +0200

    Login goes to User Menu
    main menu

--- Web-HTML-Public/Login/Login.js
+++ Web-HTML-Public/Login/Login.js
@@ -3,5 +3,10 @@ function CREATE_NEW_ACCOUNT_BUTTON(){
 	window.location.href = '../Create_Account/Create_Account.html'; // move to create account page
 
 }
+function GO_MAIN_MENU_BUTTON(){
+
+	window.location.href = '../User_Menu/User_Menu.html'; //move to login page
+
+}
 
 console.log('ahahhhhshhs');
