a2457f6b97ce2985e3bf32f2bdbc4a53cacfd379
PurpleToenail <nickgur.pass@gmail.com>
2021-11-16 22:56:33 +0100

    Cookie ready to use
    yumby

--- Back_end/frvcipher/index.js
+++ Back_end/frvcipher/index.js
@@ -1,15 +1,26 @@
-const mechanism = require("./lib/mechanism.js");
-const prompt = require('prompt-sync')({sigint: true});
+const main = require("./lib/main.js");
+const poo = require("./lib/poo.js");
 
-mechanism.hello();
+/*x = "Bananer eller dessertbananer är trivialnamn för ätliga bär som härstammar från stora blommande örter inom släktet Musa."
+cookie = poo.encrypt(x, 78);
+console.log(cookie);
+console.log(poo.decrypt(cookie, 78));*/
 
-let name = prompt("message: ");
-console.log("hello i am cool : " + name);
+function genkey (){
+    key = Math.floor(Math.random() * (310));
+    return key;
+}
 
-/*
-function encrypt (message, key) {
-  var i = 0;
-  mechanism.xyz(message);
-  return
-};
-*/
+function encrypt (plaintext, key){
+    return poo.encrypt(plaintext, key);
+}
+
+function decrypt (ciphertext, key){
+    return poo.decrypt(ciphertext, key);
+}
+
+module.exports={
+    genkey,
+    encrypt,
+    decrypt
+  };
