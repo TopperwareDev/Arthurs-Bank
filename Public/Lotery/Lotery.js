d496889322f586f344f7c332ad23dc9e44375a43
Topperware <47272494+TopperwareDev@users.noreply.github.com>
2021-11-21 12:40:24 +0100

    Further Work on lotery page

---
+++ Public/Lotery/Lotery.js
@@ -0,0 +1,21 @@
+console.log('This file if properly referenced right?');
+
+const element = document.getElementById("id01");
+element.innerHTML = (Math.round(Math.random() * 101));
+
+const d = new Date();
+
+console.log(d.getTime());
+
+console.log(d.getTime().substring(0,3));
+
+
+function countdown(hours, min, sec, text, callback){
+
+  for(i = 0; i >= 0; ++i){
+
+
+  }
+}
+
+//loteryNum.textContent = "69";
