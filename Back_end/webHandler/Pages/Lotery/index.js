e703b5931bf6a97bfa874d06b0a524a668b8e3ea
Topperware <47272494+TopperwareDev@users.noreply.github.com>
2021-12-07 11:54:47 -1100

    Lotery grid creation -> localhost/lotery

--- Back_end/webHandler/Pages/Lotery/index.js
+++ Back_end/webHandler/Pages/Lotery/index.js
@@ -4,6 +4,12 @@ const app = module.exports = express();
 const path = require('path');
 const authentication = require(path.resolve('Back_end/authentication'));
 
+// --- lotery configuration -dependant on server ram <--- will reset if server resets
+const height = 10;
+const width = 10;
+//const expireDate = ; // time until winner is selected and reset
+// ---
+
 app.get("/Lotery", (request,respond) => {
 
     authentication.validateCookie(request, (validated) =>{ //Check Authentication cookie
@@ -19,5 +25,14 @@ app.get("/Lotery", (request,respond) => {
         }
 
     });
+});
+
+app.get("/Lotery/data", (request,respond) => {
+
+     const data = [
+        {height: height, width: width}
+    ]
+    
+    respond.json(data);
 
 });
--- Public/Lotery/Lotery.js
+++ Public/Lotery/Lotery.js
@@ -1,4 +1,39 @@
 
+fetch('/Lotery/data').then(res => res.json()).then(out => gridData(out));
+
+function gridData(data){
+
+  let box = 0;
+    
+  console.log(data[0].height);
+  console.log(data[0].width);
+
+  //get table from html
+  let loteryTable = document.getElementById('loteryTable');
+
+  //generate table
+  for(let rows = 0; rows < data[0].height; ++ rows){
+
+    let row = document.createElement('tr');
+    row.id = 'row' + rows;
+
+    loteryTable.appendChild(row);
+
+    for(let cols = 0; cols < data[0].width; ++ cols){
+      
+      let col = document.createElement('td');
+      col.id = 'box' + ++box;
+      loteryTable.appendChild(col);
+    }
+  }
+
+}
+
+/*
+document.getElementById('box' + box).style.width = '10px';
+document.getElementById('box' + box).style.height = '10px';
+*/
+/*
 const element = document.getElementById("id01");
 element.innerHTML = (Math.round(Math.random() * 101));
 
@@ -18,3 +53,6 @@ function countdown(hours, min, sec, text, callback){
 }
 
 //loteryNum.textContent = "69";
+
+
+*/
